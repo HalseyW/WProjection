17f1491e1ca9e98e9a4e68db8070b7fcc432928d
halseywu <wsh940808@163.com>
2019-08-13 11:30:57 +0800

    增加了接收SSDP的方法，没有DLNA设备，暂时无法测试

--- Podfile
+++ Podfile
@@ -1,10 +1,16 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '11.0'
 
 target 'WProjection' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for WProjection
+  
+  pod 'SnapKit', '~> 5.0.0'
+  
+  pod 'AEXML'
 
+  pod 'CocoaAsyncSocket'
+  
 end
--- WProjection/ViewController.swift
+++ WProjection/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import CocoaAsyncSocket
 
 class ViewController: UIViewController {
+    let mSearchData = """
+                        M-SEARCH * HTTP/1.1
+                        MAN: "ssdp:discover"
+                        MX: 5
+                        HOST: 239.255.255.250:1900
+                        ST: ssdp:all
+                      """.data(using: .utf8)
+    //ssdp stuff
+    var ssdpAddres = "239.255.255.250"
+    var ssdpPort: UInt16 = 1900
+    var ssdpSocket: GCDAsyncUdpSocket!
+    var ssdpSocketRec: GCDAsyncUdpSocket!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ssdpSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
+        
+        try! ssdpSocket.bind(toPort: ssdpPort)
+        try! ssdpSocket.beginReceiving()
+        try! ssdpSocket.enableBroadcast(true)
+        try! ssdpSocket.joinMulticastGroup(ssdpAddres)
+        try! ssdpSocket.connect(toHost: ssdpAddres, onPort: ssdpPort)
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ssdpSocket.send(mSearchData!, toHost: ssdpAddres, port: ssdpPort, withTimeout: 1, tag: 0)
+    }
+    
+}
 
-
+extension ViewController: GCDAsyncUdpSocketDelegate {
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didConnectToAddress address: Data) {
+        print("didConnectToAddress: \(address)")
+    }
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        var host: NSString?
+        var port1: UInt16 = 0
+        GCDAsyncUdpSocket.getHost(&host, port: &port1, fromAddress: address)
+        print("From \(host!)")
+        
+        let gotData = String(data: data, encoding: .utf8)!
+        print(gotData)
+    }
+    
 }
 
